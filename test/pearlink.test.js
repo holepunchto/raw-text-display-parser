d172ffe5a7011dad8ebc712d6ded05abb8f662fa
Fred <gasolin@gmail.com>
2025-09-30 12:56:20 +0800

    fix

--- test/pearlink.test.js
+++ test/pearlink.test.js
@@ -2,11 +2,11 @@ const test = require('brittle')
 const Parser = require('..')
 
 test('setPearLink creates pear-link display entry', (t) => {
-  const parser = new Parser()
-  parser.appendText('pear://example')
-  const success = parser.setPearLink('pear://example')
+  const p = new Parser()
+  p.appendText('pear://example')
+  const success = p.setPearLink('pear://example')
   t.pass(success)
-  t.is(parser.display, {
+  t.is(p.display, {
     type: 'pear-link',
     start: 0,
     end: 14,
