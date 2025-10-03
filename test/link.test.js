0e0e6aad1142ca57f75e70fee93a5c7a4757db21
gasolin <gasolin@gmail.com>
2025-10-03 22:40:39 +0800

    [CI] test - link (#10)

---
+++ test/link.test.js
@@ -0,0 +1,20 @@
+const test = require('brittle')
+
+const Parser = require('..')
+const { DISPLAY_TYPES } = require('@holepunchto/keet-core-api')
+
+test('setLink creates http-link display entry', (t) => {
+  const p = new Parser()
+  const link = 'http://example.com'
+  p.appendText(link)
+  p.setLink(link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 0,
+      end: 18,
+      content: link,
+      length: 18
+    }
+  ])
+})
