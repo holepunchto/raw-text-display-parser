057f04fecc80f34e9f3d61532e49aadc1e346ba4
Fred <gasolin@gmail.com>
2025-12-15 16:26:29 +0800

    [link]fix - parse link with longer text

--- test/link.test.js
+++ test/link.test.js
@@ -18,3 +18,111 @@ test('setLink creates http-link display entry', (t) => {
     }
   ])
 })
+
+test('setLink with text before link', (t) => {
+  const p = new Parser()
+  const text = 'Hello http://example.com'
+  const link = 'http://example.com'
+  p.appendText(text)
+  p.setPosition(12) // position inside the link
+  p.setLink(link, link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 6,
+      end: 24,
+      content: link,
+      length: 18
+    }
+  ])
+})
+
+test('setLink with text after link', (t) => {
+  const p = new Parser()
+  const text = 'http://example.com world'
+  const link = 'http://example.com'
+  p.appendText(text)
+  p.setPosition(5) // position inside the link
+  p.setLink(link, link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 0,
+      end: 18,
+      content: link,
+      length: 18
+    }
+  ])
+})
+
+test('setLink with text before and after link', (t) => {
+  const p = new Parser()
+  const text = 'Hello http://example.com world'
+  const link = 'http://example.com'
+  p.appendText(text)
+  p.setPosition(12) // position inside the link
+  p.setLink(link, link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 6,
+      end: 24,
+      content: link,
+      length: 18
+    }
+  ])
+})
+
+test('setLink with text line before link', (t) => {
+  const p = new Parser()
+  const text = 'Hello\nhttp://example.com'
+  const link = 'http://example.com'
+  p.appendText(text)
+  p.setPosition(8) // position inside the link
+  p.setLink(link, link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 6,
+      end: 24,
+      content: link,
+      length: 18
+    }
+  ])
+})
+
+test('setLink with text line after link', (t) => {
+  const p = new Parser()
+  const text = 'http://example.com\nworld'
+  const link = 'http://example.com'
+  p.appendText(text)
+  p.setPosition(5) // position inside the link
+  p.setLink(link, link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 0,
+      end: 18,
+      content: link,
+      length: 18
+    }
+  ])
+})
+
+test('setLink with text lines before and after link', (t) => {
+  const p = new Parser()
+  const text = 'Hello\nhttp://example.com\nworld'
+  const link = 'http://example.com'
+  p.appendText(text)
+  p.setPosition(8) // position inside the link
+  p.setLink(link, link)
+  t.alike(p.display, [
+    {
+      type: DISPLAY_TYPES.HTTP_LINK,
+      start: 6,
+      end: 24,
+      content: link,
+      length: 18
+    }
+  ])
+})
